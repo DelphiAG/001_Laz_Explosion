2a2567fb47edf7ddd3bd4cf953150ffb39158938
Julius Arkenberg <julius@uni-bremen.de>
2018-09-06 17:30:46 +0200

    Kommentare hinzugefügt

--- unit1.pas
+++ unit1.pas
@@ -14,6 +14,8 @@ type
   TForm1 = class(TForm)
     DrawBtn: TButton;
     procedure DrawBtnClick(Sender: TObject);
+              //Zeichnet n Linien von der Mitte zu jeweils einem zufälligem Punkt
+              // in der Farbe C.
     procedure DrawLines(n : integer; C : TColor);
   private
 
@@ -32,10 +34,11 @@ implementation
 
 procedure TForm1.DrawBtnClick(Sender: TObject);
 begin
- DrawLines(10000,clBlack);
- DrawLines(5000,clRed);
+ DrawLines(10000,clBlack); //Aufrufen der Procedure mit verschiedenen Farben um
+ DrawLines(5000,clRed);    // eine "Explosion" zu erzeugen
  DrawLines(6000,clYellow);
  DrawLines(100000,clBlack);
+                           // Komplettes Bild wieder Schwarz setzen
  Form1.Canvas.Brush.Color:=clBlack;
  Form1.Canvas.FillRect(0,0,Form1.Width,Form1.Height);
 end;
@@ -44,13 +47,14 @@ procedure TForm1.DrawLines(n: integer; C: TColor);
 var randomPoint : TPoint;
     i :integer;
 begin
- Form1.Canvas.Pen.Color := C;
+ Form1.Canvas.Pen.Color := C; //Setzt die Farbe C als Zeichenfarbe
   randomize;
-for i := 0 to n do
+for i := 0 to n do            //For-Schleife um n-mal eine Linie zu zeichnen
   begin
+       //Zufälligen Punkt auswählen
   randomPoint.x:= random(Form1.Width);
   randomPoint.y:= random(Form1.Height);
-
+       //Von der Mitte zu diesem Punkt eine Linie zeichnen
   Form1.Canvas.MoveTo(Form1.Width div 2, Form1.Height div 2);
   Form1.Canvas.LineTo(randomPoint);
   end;
